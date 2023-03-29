92a9d6d0817c9ca07d9dcfae5fc5750d9f41fc15
arsrsyh <gotrany97@gmail.com>
2023-03-29 12:36:32 +0800

    Login Alert & Navbar Toggle

---
+++ asset/script/headerScript.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu')
+const nav = document.getElementById('navbar-toggle');
+
+menu.addEventListener('click', function () {
+    nav.classList.toggle('navbar-toggle')
+})
